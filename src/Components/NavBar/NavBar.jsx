7f82e853e41016599a4cb285046a2cf2254033d6
123Somrat <jafaruddin875@gmail.com>
2023-11-27 00:39:27 +0100

    showing navmenu conditionally

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import { MdOutlineInventory } from "react-icons/md";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Providers/Providers";
 export default function NavBar() {
-const {logOut} = useContext(AuthContext)
+const {logOut,user} = useContext(AuthContext)
 const navigate = useNavigate()
   const signOut = () =>{
     // call logout component
@@ -44,7 +44,7 @@ const navigate = useNavigate()
             <Dropdown.Item><Link to={""}>Settings</Link></Dropdown.Item>
             <Dropdown.Item>Earnings</Dropdown.Item>
             <Dropdown.Divider />
-            <Dropdown.Item onClick={signOut}>Sign out</Dropdown.Item>
+            <Dropdown.Item onClick={signOut}>Log out</Dropdown.Item>
           </Dropdown>
           <Navbar.Toggle />
         </div>
@@ -52,8 +52,8 @@ const navigate = useNavigate()
           <Link href="/" className="hover:text-cyan-800">
             Home
           </Link>
-          <Link to={"/auth/login"} className="hover:text-cyan-800">Login</Link>
-          <Link to="/auth/register" className="hover:text-cyan-800">Register</Link>
+          {user ? <p className="hover:text-cyan-800" onClick={signOut}>Logout</p> : <Link to={"/auth/login"} className="hover:text-cyan-800">Login</Link> }
+          {user ?<Link to={"/dashboard"} className="hover:text-cyan-800">Dashboard</Link> :  <Link to="/auth/register" className="hover:text-cyan-800">Register</Link>}
           <Link to="/createshop" className="hover:text-cyan-800">Create Store</Link>
           <Link to="/watchdemo" className="hover:text-cyan-800">Watch Demo</Link>
         </Navbar.Collapse>
