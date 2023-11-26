c21b68b2a5c337d3b75524aad4f775f77f86d132
123Somrat <jafaruddin875@gmail.com>
2023-11-26 12:41:48 +0100

    add validation on register form

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,13 @@
-import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { useForm } from "react-hook-form";
 export default function Register() {
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+
+
+
   return (
     <div className="max-w-6xl mx-auto m-8">
       <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-900 dark:text-gray-100 mx-auto m-8">
@@ -11,7 +17,7 @@ export default function Register() {
             Register in to access our feature
           </p>
         </div>
-        <form className="space-y-12">
+        <form className="space-y-12" onSubmit={handleSubmit(onSubmit)}>
           <div className="space-y-4">
             <div>
               <label htmlFor="email" className="block mb-2 text-sm">
@@ -23,7 +29,9 @@ export default function Register() {
                 id="username"
                 placeholder="mohammad"
                 className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:border-cyan-700"
+                {...register("username",{required:true,minLength:5})}
               />
+              {errors.username && <span className="text-red-700 mt-2 block ml-1">username is required</span>}
             </div>
             <div>
               <label htmlFor="email" className="block mb-2 text-sm">
@@ -35,7 +43,10 @@ export default function Register() {
                 id="email"
                 placeholder="leroy@jenkins.com"
                 className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:border-cyan-700"
+                {...register("email",{required:true,pattern:/.+\@.+\..+/})}
+              
               />
+              {errors.email && <span className="text-red-700 mt-2 block ml-1">email is required and must follow email pattern</span>}
             </div>
             <div>
               <div className="flex justify-between mb-2">
@@ -49,7 +60,9 @@ export default function Register() {
                 id="password"
                 placeholder="*****"
                 className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:border-cyan-700"
+                {...register("password",{required:true,minLength:7})}
               />
+              {errors.password && <span className="text-red-700 mt-2 block ml-1">password must be 7 cherecter long</span>}
             </div>
           </div>
           <div className="space-y-2">
