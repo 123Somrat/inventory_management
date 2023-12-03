b84fc227f0bad3bf5c0fb28abb8dfe10853c79c2
123Somrat <jafaruddin875@gmail.com>
2023-12-04 00:40:24 +0100

    show all users for admin

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import { useContext, useEffect, useState } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure"
+import { AuthContext } from "../../Providers/Providers";
+import { Button, Table } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function Users() {
+    const axiosbaseUrl = useAxiosSecure();
+    const {user} = useContext(AuthContext);
+    const [users,setUsers] = useState([])
+   useEffect(()=>{
+       axiosbaseUrl.get("/allusers")
+       .then(res=>setUsers(res.data))
+
+
+   },[])
+
+
   return (
-    <div>Users</div>
+    <div>
+            <Table className='drop-shadow-none'>
+         <Table.Head>
+           
+           <Table.HeadCell>User Name</Table.HeadCell>
+    
+           <Table.HeadCell>User Email</Table.HeadCell>
+           <Table.HeadCell>Created At</Table.HeadCell>
+           <Table.HeadCell>Role</Table.HeadCell>
+           <Table.HeadCell>Send Promotional Email</Table.HeadCell>
+         </Table.Head>
+         <Table.Body className="divide-y border-none">
+           {
+            users.map((user,id)=><Table.Row key={id} className="bg-white dark:border-gray-700 dark:bg-gray-80">
+            <Table.Cell>{user?.userName
+}</Table.Cell>
+            <Table.Cell  >{user?.email}</Table.Cell>
+            <Table.Cell>{user?.createdAt}</Table.Cell>
+            <Table.Cell>
+            <Table.Cell>{user?.role ? user.role :"null"}</Table.Cell>
+            </Table.Cell>
+            <Table.Cell>
+              <p className="font-medium text-red-600 hover:underline dark:text-cyan-500">
+               <Button>Send Email</Button>
+              </p>
+            </Table.Cell>
+          </Table.Row>)
+
+
+           }
+         </Table.Body>
+         </Table>
+    </div>
   )
 }
