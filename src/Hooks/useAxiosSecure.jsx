f3857e8f79a768b42ef0a45835fe9753c9cad58b
123Somrat <jafaruddin875@gmail.com>
2023-12-13 21:38:53 +0100

    set the jwt token in axios interceptor headers

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,13 +1,34 @@
 import axios from "axios";
 
 export default function useAxiosSecure() {
+
   const axiosbaseUrl = axios.create({
     baseURL: "http://localhost:5000",
   });
 
-  axios.interceptors.response.use(function (response) {
+  
+  axiosbaseUrl.interceptors.request.use(function (config) {
+    // get token from localStorage
+       const token = localStorage.getItem("jwt_token");
+     
+       // set the token in headers
+        config.headers.Authorization = `Bearer ${token}`
+
+    // Do something before request is sent
+    return config;
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+
+
+
+  // response interceptor
+  axiosbaseUrl.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+   
     return response;
   }, function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
